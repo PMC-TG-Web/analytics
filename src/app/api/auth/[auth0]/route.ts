54ea048d293be4c5c180173459b05fdc520a6be1
Todd Gilmore <todd@pmcdecor.com>
2026-02-18 07:06:59 -0500

    Fix Auth0Client type error in build

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,4 +1,4 @@
 import { auth0 } from '@/lib/auth0';
 
-export const GET = auth0.handler;
-export const POST = auth0.handler;
+export const GET = auth0.handleAuth();
+export const POST = auth0.handleAuth();

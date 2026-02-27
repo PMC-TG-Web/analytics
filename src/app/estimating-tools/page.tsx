50989985a07d83d2a04d5d2b35f6aeab79c05c11
Todd Gilmore <todd@pmcdecor.com>
2026-02-27 08:38:15 -0500

    Add orderBy import for estimating tools

--- src/app/estimating-tools/page.tsx
+++ src/app/estimating-tools/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { db, getDocs, query, collection, where, addDoc, limit } from "@/firebase";
+import { db, getDocs, query, collection, where, addDoc, limit, orderBy } from "@/firebase";
 
 import ProtectedPage from "@/components/ProtectedPage";
 import Navigation from "@/components/Navigation";

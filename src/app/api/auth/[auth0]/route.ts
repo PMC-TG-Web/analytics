9306887f1258162f03fd76f8a4ee9cfba61f3e46
Todd Gilmore <todd@pmcdecor.com>
2026-03-18 14:10:06 -0400

    fix: use auth0.middleware (not .handler) for Auth0 catch-all route

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,4 +1,4 @@
 import { auth0 } from '@/lib/auth0';
 
-export const GET = auth0.handler;
-export const POST = auth0.handler;
+export const GET = auth0.middleware;
+export const POST = auth0.middleware;
